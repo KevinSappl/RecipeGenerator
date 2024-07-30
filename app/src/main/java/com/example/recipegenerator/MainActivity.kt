8bdec030bc4f731cc885f33855df49cbf6016165
MarioRS21 <mario@mariodittrich.com>
2024-07-30 23:20:04 +0200

    Added MainActivity with Logo, Text (with animation), Credits and relinks to MainMenuActivity.

--- app/src/main/java/com/example/recipegenerator/MainActivity.kt
+++ app/src/main/java/com/example/recipegenerator/MainActivity.kt
@@ -5,6 +5,12 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import android.content.Intent
+import android.os.Handler
+import android.os.Looper
+import android.widget.ImageView
+import android.widget.TextView
+import android.view.animation.AnimationUtils
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,5 +22,22 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        val logo = findViewById<ImageView>(R.id.logo)
+        val appName = findViewById<TextView>(R.id.appName)
+        val creatorName = findViewById<TextView>(R.id.creatorName)
+
+
+        val fadeInAnimation = AnimationUtils.loadAnimation(this, R.anim.fade_in)
+        logo.startAnimation(fadeInAnimation)
+
+
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            startActivity(Intent(this, MainMenuActivity::class.java))
+            finish()
+        }, 2000)
     }
+
 }
+
