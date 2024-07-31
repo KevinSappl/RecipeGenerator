fc3e382cf4fc0f5ea57304ad9c83cc312d79c18c
yahorpaulson <y.paulson@yahoo.com>
2024-07-31 14:18:23 +0200

    buttons in MainMenuActivity are connected

--- app/src/main/java/com/example/recipegenerator/MainMenuActivity.kt
+++ app/src/main/java/com/example/recipegenerator/MainMenuActivity.kt
@@ -3,6 +3,7 @@ package com.example.recipegenerator
 import android.content.Intent
 import android.os.Bundle
 import android.widget.ArrayAdapter
+import android.widget.Button
 import android.widget.ListView
 import androidx.appcompat.app.AppCompatActivity
 import com.example.recipegenerator.recipeDB.Recipe
@@ -15,15 +16,34 @@ import kotlinx.coroutines.withContext
 class MainMenuActivity : AppCompatActivity() {
     private lateinit var recipesListView: ListView
     private val coroutineScope = CoroutineScope(Dispatchers.Main)
+    private lateinit var btnFoods: Button
+    private lateinit var btnScan: Button
+    private lateinit var btnFavourites: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main_menu)
 
         recipesListView = findViewById(R.id.recipesList)
+        btnFoods = findViewById(R.id.btnFoods)
+        btnScan = findViewById(R.id.btnScan)
+
 
         // aufladen Rezepten aus DB
         loadRecipes()
+
+        btnFoods.setOnClickListener {
+            val intent = Intent(this, GroceriesOperationsActivity::class.java)
+            startActivity(intent)
+        }
+
+        btnScan.setOnClickListener{
+            val intent = Intent(this, FoodScanner::class.java)
+            startActivity(intent)
+        }
+        btnFavourites.setOnClickListener{
+            //TODO:Favourites Activity
+        }
     }
 
     // RezeptenList dynamisch auffüllen
@@ -41,8 +61,12 @@ class MainMenuActivity : AppCompatActivity() {
         val adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, recipeNames)
         recipesListView.adapter = adapter
 
-        //recipesListView.setOnItemClickListener()
-
-        //hier beim klicken wird Activity mit dem ausgewälten Rezept aufgerufen (activity_recipe_details.xml)
+        recipesListView.setOnItemClickListener { _, _, position, _ ->
+            val selectedRecipe = recipes[position]
+            val intent = Intent(this, RecipeDetailActivity::class.java).apply {
+                putExtra("recipe_id", selectedRecipe.id)
+            }
+            startActivity(intent)
+        }
     }
 }
