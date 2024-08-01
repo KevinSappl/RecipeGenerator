21fe73232e2fa11c74f2e7f81541821b0dd177a1
fabianberchtold <127214759+fabianberchtold@users.noreply.github.com>
2024-08-01 15:57:47 +0200

    update gradle dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -83,7 +83,6 @@ dependencies {
     implementation(libs.androidx.ui)
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
-    implementation(libs.androidx.material3)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
@@ -103,7 +102,6 @@ dependencies {
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.5.1")
     implementation("androidx.activity:activity-compose:1.6.1")
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.5.1")
-    implementation("androidx.compose.material3:material3")
     implementation("androidx.compose.ui:ui")
     // preview
     implementation("androidx.compose.ui:ui-tooling-preview")
@@ -118,8 +116,6 @@ dependencies {
 
     implementation("androidx.lifecycle:lifecycle-runtime-compose:2.6.0")
     implementation("androidx.activity:activity-compose:1.7.0")
-    implementation("androidx.compose.material3:material3:1.0.0")
-    implementation("androidx.compose.material3:material3:1.0.0")
     implementation("androidx.compose.ui:ui:1.4.3")
     implementation("androidx.compose.ui:ui-tooling-preview:1.4.3")
 
