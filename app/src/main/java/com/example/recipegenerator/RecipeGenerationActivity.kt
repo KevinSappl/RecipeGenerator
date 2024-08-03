19d1dc2d7647b0e43f72288c17f67e78e73ca8bc
fabianberchtold <127214759+fabianberchtold@users.noreply.github.com>
2024-08-03 15:40:32 +0200

    add ui for recipe

--- app/src/main/java/com/example/recipegenerator/RecipeGenerationActivity.kt
+++ app/src/main/java/com/example/recipegenerator/RecipeGenerationActivity.kt
@@ -5,16 +5,19 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material.FloatingActionButton
 import androidx.compose.material.Scaffold
 import androidx.compose.material.Switch
 import androidx.compose.material.SwitchDefaults
 import androidx.compose.material.Text
 import androidx.compose.material.TopAppBar
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Edit
 import androidx.compose.material.icons.rounded.SmartToy
 import androidx.compose.material3.BottomAppBar
 import androidx.compose.material3.ExtendedFloatingActionButton
@@ -27,13 +30,19 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.drawBehind
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.onSizeChanged
+import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.text.AnnotatedString
 import androidx.compose.ui.text.SpanStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.example.recipegenerator.recipeDB.Recipe
 import com.example.recipegenerator.recipegenerator.RecipeGenerator
 import com.example.recipegenerator.ui.theme.Orange
 import com.example.recipegenerator.ui.theme.Pink
@@ -134,6 +143,79 @@ fun GroceryItem(grocery: GroceryItem){
     Text(grocery.name, fontSize = 18.sp, modifier = Modifier.padding(top = 10.dp, bottom = 10.dp),
         color = Color.White, fontWeight = FontWeight.Bold)
     HorizontalDivider()
+}
+
+
+@Composable
+fun RecipeDetails(recipe: Recipe) {
+    //Inside the "drawBehind" scope I cannot access colorscheme, that's why I need
+    //to get the secondary color here.
+    val secondaryColor = PinkOrangeHorizontalGradient;
+    //This state variable is needed to get size of the border rectangles
+    //I do not know if there is a better way of doing this.
+    var borderVerticalWidth by remember { mutableStateOf(0f) }
+
+    Scaffold(topBar = {
+        TopAppBar(
+            title = { Text(text = recipe.name) },
+            modifier = Modifier.background(PinkOrangeHorizontalGradient),
+            backgroundColor = Color.Transparent,
+            contentColor = Color.White
+        )
+
+
+    },
+        modifier = Modifier
+            .background(Color.White)
+            .drawBehind { //Here I draw the lines on the sides and top+bottom of the background
+                //Sadly I have not found an easier way to do this.
+                drawRect(brush = secondaryColor, size = Size(size.width / 10f, size.height))
+                drawRect(
+                    brush = secondaryColor,
+                    size = Size(borderVerticalWidth, size.height),
+                    topLeft = Offset(size.width - borderVerticalWidth, 0f)
+                )
+                drawLine(
+                    brush = secondaryColor,
+                    start = Offset(0f, size.width / 20f),
+                    end = Offset(size.width, size.width / 20f),
+                    strokeWidth = size.width / 10f
+                )
+                drawLine(
+                    brush = secondaryColor,
+                    start = Offset(0f, size.height),
+                    end = Offset(size.width, size.height),
+                    strokeWidth = borderVerticalWidth
+                )
+            }
+            .padding(top = 20.dp)
+            .onSizeChanged { size ->
+                borderVerticalWidth = size.width / 10f;
+            },
+        backgroundColor = Color.Transparent,
+        floatingActionButton = {
+            FloatingActionButton(
+                onClick = { /*TODO*/ },
+                backgroundColor = Orange
+            )
+
+            {
+                Icon(Icons.Default.Edit, "Edit Recipe", tint = Color.White)
+            }
+        }) { innerPadding ->
+        Column(
+            Modifier
+                .padding(innerPadding)
+                .padding(
+                    start = (borderVerticalWidth / LocalDensity.current.density).dp,
+                    end = (borderVerticalWidth / LocalDensity.current.density).dp,
+                    bottom = (borderVerticalWidth / 2 / LocalDensity.current.density).dp
+                )
+        ) {
+            Text(recipe.details)
+        }
+
+    }
 }
 
 @Preview(showBackground = true)
